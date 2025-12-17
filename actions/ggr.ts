239e107d0ebd90fcc163b638de9628230cbc947f
Fillip Mangia <fillipgms@gmail.com>
2025-12-17 16:22:32 -0300

    zoom when edditing user fix

--- actions/ggr.ts
+++ actions/ggr.ts
@@ -66,6 +66,9 @@ export async function createGGR(formData: {
         redirect("/login");
     }
 
+    console.log(`Bearer ${session.accessToken}`);
+    console.log(formData);
+
     try {
         const { data } = await axios.post(
             `${process.env.API_ROUTES_BASE}/ggr`,
